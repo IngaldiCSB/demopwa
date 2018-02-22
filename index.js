bdd22c9aaf9a1dcfb34fa23055b2a7b5199dcb58
carmine ingaldi <cingaldi@scmgroup.com>
2018-02-22 05:00:50 -0800

    added engine attribute to package.json(fix)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 // Define the port to run on
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 app.use(express.static(__dirname + '/src'));
 
