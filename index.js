a4a8d03159428dd007dc7884664d432a29881de9
carmine ingaldi <cingaldi@scmgroup.com>
2018-02-22 03:51:32 -0800

    added static file access and index page (fix)

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/src'));
 
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
